008e0ef1f05e8133da179477275cd763d7cda7a6
JAVIER NOVOA <javier.novoa@walmart.com>
2022-12-12 15:41:46 -0300

    chore: database ops types refactor

--- src/app/infrastructure/database/mocks/index.d.ts
+++ src/app/infrastructure/database/mocks/index.d.ts
@@ -1,8 +1,3 @@
-import { LevelModel } from '../../input/level/interfaces'
-import { PaymentModel } from '../../input/payments/interfaces'
-import { PurchaseModel } from '../../input/purchases/interfaces'
-import { RestrictionModel } from '../../input/restrictions/interfaces'
-import { ShipmentModel } from '../../input/shipment/interfaces'
 import { UserModel } from '../../input/user/interfaces'
 
 export default class MockUtils {
@@ -14,14 +9,4 @@ export default class MockUtils {
   ): Promise<any>
 
   getUser(): Promise<UserModel>
-
-  getUserRestrictions(userId: string): Promise<RestrictionModel[]>
-
-  getUserPurchases(userId: string): Promise<PurchaseModel[]>
-
-  getLevel(levelId: string): Promise<LevelModel>
-
-  getShipment(shipmentId: string): Promise<ShipmentModel>
-
-  getPayment(paymentId: string): Promise<PaymentModel>
 }
